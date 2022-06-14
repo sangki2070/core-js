18b483e57c19805164e8ac96efe7bd275c7b8ff5
sangki2070 <89248004+sangki2070@users.noreply.github.com>
2022-06-14 23:32:10 +0900

    add callback function

---
+++ callback/index.js
@@ -0,0 +1,35 @@
+// ******* 제어권 위임의 예시 *******
+//setInterval
+
+var cb = function () {
+  console.log("초마다 실행");
+};
+
+setInterval(cb, 1000);
+
+//setInterval에게 cb의 제어권을 넘겨줘서 setInterval이 실행함
+
+// ******* 매개변수의 예시 *******
+
+var arr = [1, 2, 3, 4, 5];
+
+var entries = [];
+
+arr.forEach(
+  function (v, i) {
+    entries.push([i, v, this[i]]);
+  },
+  [10, 20, 30, 40, 50]
+);
+
+console.log(entries);
+
+// this
+
+document.body.innerHtmL = '<div id="a">abc</div>';
+
+function cbFunc() {
+  console.log(this, x);
+}
+
+document.getElementById("a").addEventListener("click", cbFunc);
